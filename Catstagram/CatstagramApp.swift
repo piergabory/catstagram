f593151fc5d1cfc663b55d721eb44c9ded346b1b
Pierre Gabory <pierre.gabory@legrand.com>
2025-07-06 15:02:51 +0200

    Initial Commit

---
+++ Catstagram/CatstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatstagramApp.swift
+//  Catstagram
+//
+//  Created by Pierre GABORY on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CatstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
